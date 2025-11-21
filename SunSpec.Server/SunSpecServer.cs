5d25b7e7e29270d0df6fcc8c7a90245c6cc69cea
Gareth Potter <gareth.potter@brillpower.com>
2025-11-21 15:35:38 +0000

    server: log model length during registration

--- SunSpec.Server/SunSpecServer.cs
+++ SunSpec.Server/SunSpecServer.cs
@@ -68,7 +68,7 @@ public class SunSpecServer : IDisposable
                 if (builder.Build(holdingRegisters.Slice(_currentRegister * 2), out int length, out ISunSpecModel model))
                 {
                     _modelsByStartingRegister.Add(_currentRegister, model);
-                    _logger?.LogInformation($"Registered model {model.GetType().Name} (ID {model.ID}) at register {_currentRegister}.");
+                    _logger?.LogInformation($"Registered model {model.GetType().Name} (ID {model.ID}) of length {model.Length} at register {_currentRegister}.");
                     _currentRegister += length;
                 }
             }
